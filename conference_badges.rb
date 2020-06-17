356aedafe194a89fd8edb6085626f3ac5699807b
Alice <agenkin1@gmail.com>
2020-06-17 16:26:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  newArray = []
+  names.each do |name|
+    newArray.push("Hello, my name is #{name}.")
+  end
+  return newArray
+end
+
+def assign_rooms(names)
+newArray1 =[]
+room = 0
+  names.each do |name|
+  newArray1.push ("Hello, #{name}! You'll be assigned to room #{room}!")
+    room += 1
+  end
+end
+
+printer
