eeae1c29fa266d77f4b1cc6a741f422a19e4ed97
Alice <agenkin1@gmail.com>
2020-06-17 18:03:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,13 +12,13 @@ end
 
 
 def assign_rooms(names)
-counter = 0
-array1 =[]
-while counter > 8
-  names.each do |name|
-    array1.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter+=1
-end
-end
-return array1
+  counter = 0
+  array1 =[]
+  while counter < 8 do
+    names.each do |name|
+      array1.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+      counter+=1
+    end
+  end
+  array1
 end
