7a314281e36810097448a8f9727164655b967ffb
Alice <agenkin1@gmail.com>
2020-06-17 16:27:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,9 +14,7 @@ def assign_rooms(names)
 newArray1 =[]
 room = 0
   names.each do |name|
-  newArray1.push ("Hello, #{name}! You'll be assigned to room #{room}!")
+  newArray1.push("Hello, #{name}! You'll be assigned to room #{room}!")
     room += 1
   end
 end
-
-printer
