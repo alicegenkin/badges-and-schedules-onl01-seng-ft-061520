4660cb3b9a23ba858e81e0e9f2e9715d598e511d
Alice <agenkin1@gmail.com>
2020-06-17 16:28:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(names)
 room = 0
   names.each do |name|
-  "Hello, #{name}! You'll be assigned to room #{room}!"
+  return "Hello, #{name}! You'll be assigned to room #{room}!"
     room += 1
   end
 end
